2c8d5e084c7c7ab8df75306b8f72ba28a4ab8c14
lzhhe <13071108606@163.com>
2023-10-26 14:10:33 +0800

    some modify about database

--- App/__init__.py
+++ App/__init__.py
@@ -2,6 +2,13 @@ import datetime
 
 from flask import Flask
 from .views import blue
+from .extents import init_exts
+
+HOSTNAME = "127.0.0.1"
+PORT = 3306
+USERNAME = "root"
+PASSWORD = "131a2abLZH"
+FLASK_DB = "calendar_web"
 
 
 def create_app():
@@ -12,5 +19,11 @@ def create_app():
 
     app.config['SECRET_KEY'] = 'COMP3019J'
     # app.config['PERMANENT_SESSION_LIFETIME'] = datetime.timedelta(days=7)
+
+    db_uri = f'mysql+pymysql://{USERNAME}:{PASSWORD}@localhost:{PORT}/{FLASK_DB}?charset=utf8mb4'
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    init_exts(app)
 
     return app
--- app.py
+++ app.py
@@ -5,6 +5,3 @@ app = create_app()
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
 
-
-def db():
-    return None
